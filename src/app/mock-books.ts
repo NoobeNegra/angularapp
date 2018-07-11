bade16d24f7e675de253ab6bc828a6ae49999a70
NoobeNegra <morenoailin@gmail.com>
2018-07-11 10:36:12 +0200

    Update mock-books.ts

--- src/app/mock-books.ts
+++ src/app/mock-books.ts
@@ -1,8 +1,8 @@
 import { Book } from './book';
 
 export const BOOKS: Book[] = [
-  { id: 1, name: 'Casas Muertas', author:'Miguel Otero Silva', quote:'Una casa muerta entre mil casas muertas', description:'Escrita en 1900'},
-  { id: 2, name: 'Casas Muertas', author:'Miguel Otero Silva', quote:'Una casa muerta entre mil casas muertas', description:'Escrita en 1900'},
+  { id: 1, name: 'Casas Muertas', author:'Miguel Otero Silva', quote:'Una casa muerta, entre mil casas muertas, mascullando el mensaje desesperado de una época desaparecida', description:'Casas Muertas es la segunda novela del escritor venezolano Miguel Otero Silva, fue publicada por Editorial Losada, en el año 1955'},
+  { id: 2, name: 'Las lanzas coloradas', author:'Arturo Uslar Pietri', quote:'¡Noche oscura! Venía chorreando el agua, chorreando, chorreando, como si ordeñaran el cielo', description:'Las lanzas coloradas es una novela del escritor venezolano Arturo Uslar Pietri, escrita en París en 1929/1930 y publicada en 1931 en Madrid (Editorial Zeus), en la cual relata un episodio de la guerra de independencia de Venezuela, cuando la región del llano fue asolada por el general realista Boves'},
   { id: 3, name: 'Casas Muertas', author:'Miguel Otero Silva', quote:'Una casa muerta entre mil casas muertas', description:'Escrita en 1900'},
   { id: 4, name: 'Casas Muertas', author:'Miguel Otero Silva', quote:'Una casa muerta entre mil casas muertas', description:'Escrita en 1900'},
   { id: 5, name: 'Casas Muertas', author:'Miguel Otero Silva', quote:'Una casa muerta entre mil casas muertas', description:'Escrita en 1900'},
